6c7641ff01a1d9d26975d638927b1e0a518c69f2
Timo Kempf <124527843+Timokinator@users.noreply.github.com>
2023-10-15 22:00:42 +0200

    imprint + data-protection

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,4 +8,8 @@ import { Component, Input } from '@angular/core';
 export class MenuComponent {
   @Input() darkMode = true;
 
+
+  
+
+
 }
--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.scss']
 })
-export class SlideshowComponent {
+export class SlideshowComponent implements OnInit {
   images = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg']
   headlines = ['Bring engineering to the next level',
     'Code, Create, Innovate',
